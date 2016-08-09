e19d319d04d448817a750b87ece1aa3da4cea081
Tianon Gravi <admwiggin@gmail.com>
2016-08-09 12:40:40 -0700

    Convert "generate-stackbrew-library.sh" to output the new 2822-based format

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -3,14 +3,15 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-url='git://github.com/RocketChat/Docker.Official.Image'
+url='https://github.com/RocketChat/Docker.Official.Image.git'
 
 commit="$(git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
 fullVersion="$(grep -m1 'ENV RC_VERSION ' ./Dockerfile | cut -d' ' -f3)"
 
-echo '# maintainer: Rocket.Chat Image Team <buildmaster@rocket.chat>'
-echo
-echo "$fullVersion: ${url}@${commit}"
-echo "${fullVersion%.*}: ${url}@${commit}"
-echo "${fullVersion%.*.*}: ${url}@${commit}"
-echo "latest: ${url}@${commit}"
+cat <<EOF
+Maintainers: Rocket.Chat Image Team <buildmaster@rocket.chat> (@RocketChat)
+GitRepo: $url
+
+Tags: $fullVersion, ${fullVersion%.*}, ${fullVersion%.*.*}, latest
+GitCommit: $commit
+EOF
